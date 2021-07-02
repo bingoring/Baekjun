6f199292cf9c8f2f38f456cd82055f99344d595c
bingoring <ritch1530@gmail.com>
2021-07-02 21:36:02 +0900

    array/3273

---
+++ array/3273.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int arr[1000000];
+	
+	int num;
+	cin >> num;
+
+	for(int i = 0; i < num; i++){
+		int j;
+		cin >> j;
+		arr[i] = j;
+	}
+	
+	int m;
+	cin >> m;
+	
+	int _arr[1000000];
+	int count;
+	for(int i = 0; i < num; i++){
+		if(_arr[m - arr[i]] == 1) count++;
+		_arr[arr[i]] = 1;
+	}
+	cout << count;
+	return 0;
+}
